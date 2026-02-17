9d45fb2b09eaf56f798dd520d67489a686fb97f9
Ryan <Woodedcode@gmail.com>
2026-02-16 22:00:50 -0600

    Add files via upload

---
+++ python-mastery/fundamentals/numbers.py
@@ -0,0 +1,23 @@
+age = 30
+print(age)
+
+#
+# ways to write variables
+# camelCase
+#PascalCase
+# snake_case 
+
+# Arithmetic
+result = round(20 / 3 )
+print(result)
+result1 = 5 % 3
+print(result1)
+
+age = 300 
+age /= 9
+print(age)
+age += 1
+age += 5
+print(age)
+
+
