b34689d04325eae0930c72e71c23f1a6b7f424cb
Ryan <Woodedcode@gmail.com>
2026-02-18 21:17:15 -0600

    Add files via upload

---
+++ python-mastery/fundamentals/logic.py
@@ -0,0 +1,35 @@
+# happy = True
+# print(happy)
+
+# print(5 < 3)
+
+
+# me = "Nerd"
+# you = "Nerd"
+# print(me == you)
+
+
+# my_grades = [100,99,98]
+# your_grades = my_grades[:]
+# print(my_grades == your_grades)
+# print(my_grades is your_grades)
+# print(id(my_grades), id(your_grades))
+# print(id(my_grades) == id(your_grades))
+
+
+print("A before B?", "ABC" < "BCD")
+print("AZC" < "ACD")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
