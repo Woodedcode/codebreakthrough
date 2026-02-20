c2f4f8b4e25c2bfca96966395951fe1429f28a72
Ryan <Woodedcode@gmail.com>
2026-02-19 22:03:56 -0600

    Add files via upload

---
+++ python-mastery/fundamentals/loops.py
@@ -0,0 +1,38 @@
+# languages = ["Go", "Zig", "Rust"]
+
+# print(languages)
+
+
+# for language in languages:
+#     print(language, end=" ")
+# print()
+
+
+# for i in range(10):
+#     print(i)
+
+# for i, language in enumerate(languages):
+#     print(i, language, end=" ")
+# print()
+
+# for i in range(len(languages)):
+#     print(i, languages[i], end=" ")
+# print()
+
+# print(languages[0])
+# for i in range(0, len(languages)-1):
+#     print(f"{languages[i]}, next: {languages[i+1]}")
+# print(languages[-1])
+
+
+languages = ["Go", "Zig", "Rust", "C#", "Java", "F#"]
+
+for i in range(len(languages)-1, -1, -1):
+    print(languages[i])
+
+
+
+
+
+
+
